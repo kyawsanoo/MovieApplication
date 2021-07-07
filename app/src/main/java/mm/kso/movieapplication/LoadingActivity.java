f2389bdda305f08f2d133b8793e6824eaaeeff4c
kyawsanoo <kyawsanoo.androider@gmail.com>
2021-07-07 20:43:32 +0630

    add loading screen

---
+++ app/src/main/java/mm/kso/movieapplication/LoadingActivity.java
@@ -0,0 +1,31 @@
+package mm.kso.movieapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+
+public class LoadingActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        ImageView imageView=findViewById(R.id.image_loading);
+        Glide.with(getBaseContext()).load(R.raw.marvel).into(imageView);
+        startLoading();
+    }
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(LoadingActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();            }
+        }, 4600);
+    }
+}
